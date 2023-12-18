9e7edf5982166bd8e0d2beb15088c8080d6ec257
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-12-18 07:17:14 -0500

    Add files via upload

--- pages/components/header.js
+++ pages/components/header.js
@@ -19,18 +19,24 @@ function HeaderPiece() {
   return (
     <>
     <div className={styles.header_wrapper}>
+
     <ul className={styles.headercomponent}>
+
         <li className={styles.link_top_prt1}> 
         <Link className={styles.header_link} href={"/"} > Home </Link></li>
+
         <li className={styles.header_logo}>
       <Image src={"/logo.jpg"} width={100} height={100} alt="economyblindsandshades_logo" />
     </li>
+
     <li className={styles.link_top_prt2}> 
     <Link className={styles.header_link} href={"/privacypolicy"} > Privacy Policy </Link>
      <Link className={styles.header_link} href={"/getaquote"} > Get A Quote </Link>
      <Link className={styles.header_link} href={"/learnmore"} > Learn More </Link>
     </li>
+
     </ul>
+    
     </div>
     </>
   );
