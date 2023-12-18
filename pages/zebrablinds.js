90268296cc825c6a1a02111e965bd8d64828c615
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-12-18 07:24:32 -0500

    Add files via upload

---
+++ pages/zebrablinds.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import HeaderPiece from "./components/header";
+import ZebrablindsPage from "./components/pages/zebrablinds";
+function zebrablinds () { 
+
+
+    return ( 
+        <>
+
+        <HeaderPiece></HeaderPiece>
+        <ZebrablindsPage></ZebrablindsPage>
+
+
+        </>
+
+    )
+}
+
+
+export default zebrablinds;
