c69f9d8923a8f38f47c432bf6126d36e9aa5c221
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-12-21 18:21:35 -0500

    Update materials.js

--- pages/components/materials/materials.js
+++ pages/components/materials/materials.js
@@ -1,20 +1,19 @@
-
-
-// materials.js
-export const imagesData = [
-    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
-    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
-    
-    // Add more image data as needed
-  ];
+const imagesData = () => {
   
+  return [
+    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_26_01', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_2', src: '/etb3004.jpg', label: 'ETB300-41' },
+    { key: 'ema_601_1', src: '/etb3004.jpg', label: 'ETB300-41' },
+  ];
+};
+
+export default imagesData;
