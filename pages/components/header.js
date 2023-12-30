df7ca1b77e4ddf1ea7f1f6b430a4d730f6281f7b
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-12-30 17:43:29 -0500

    Update header.js

--- pages/components/header.js
+++ pages/components/header.js
@@ -21,24 +21,71 @@ function HeaderPiece() {
     <div className={styles.header_wrapper}>
 
     <ul className={styles.headercomponent}>
-
         <li className={styles.link_top_prt1}> 
         <Link className={styles.header_link} href={"/"} > Home </Link></li>
-
         <li className={styles.header_logo}>
           <Link href={'/'}>
       <Image className={styles.logo_image} src={"/logo.jpg"} width={400} height={400} alt="economyblindsandshades_logo" />
       </Link>
     </li>
-
     <li className={styles.link_top_prt2}> 
     <Link className={styles.header_link} href={"/privacypolicy"} > Privacy Policy </Link>
      <Link className={styles.header_link} href={"/getaquote"} > Get A Quote </Link>
      <Link className={styles.header_link} href={"/learnmore"} > Learn More </Link>
     </li>
-
     </ul>
-    
+
+
+    <ul className={styles.second_menu_bar}>
+
+      <li className={styles.second_menu_bar_item}>Zebra Blinds
+      <Image 
+      className={styles.chevron_top_menu}
+      width={100}
+      height={100}
+      src={"/chevronwhite.png"}
+      alt="chevron_top_menu">
+      </Image>
+      </li>
+
+      <li className={styles.second_menu_bar_item}>Shangrila Blinds
+      <Image 
+      className={styles.chevron_top_menu}
+      width={100}
+      height={100}
+      src={"/chevronwhite.png"}
+      alt="chevron_top_menu">
+      </Image>
+      </li>
+
+      <li className={styles.second_menu_bar_item}>Roman Blinds
+      <Image 
+      className={styles.chevron_top_menu}
+      width={100}
+      height={100}
+      src={"/chevronwhite.png"}
+      alt="chevron_top_menu">
+      </Image></li>
+
+      <li className={styles.second_menu_bar_item}>Roller Blinds
+      <Image 
+      className={styles.chevron_top_menu}
+      width={100}
+      height={100}
+      src={"/chevronwhite.png"}
+      alt="chevron_top_menu">
+      </Image></li>
+
+      <li className={styles.second_menu_bar_item}>HoneyComb Blinds
+      <Image 
+      className={styles.chevron_top_menu}
+      width={100}
+      height={100}
+      src={"/chevronwhite.png"}
+      alt="chevron_top_menu">
+      </Image>
+      </li>
+    </ul>
     </div>
     </>
   );
