236c2f233745e581c8ecd4f2e2976843ac8889da
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2024-02-08 01:05:50 -0500

    Add files via upload

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,8 +8,8 @@ import GoogleProvider from 'next-auth/providers/google';
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET, 
