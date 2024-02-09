e626e26bd2efd3dee2b09f5e730432cb58ab463e
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2024-02-09 04:57:29 -0500

    Delete pages directory

--- pages/api/auth/route.js
+++
@@ -1,8 +0,0 @@
-//C:\Users\User\economyblindsandshadesjs\pages\api\auth\route.js
-
-import NextAuth from "next-auth/next";
-import { config } from "../../../auth";
-
-const handler = NextAuth(config);
-export { handler as GET, handler as POST };
-s
