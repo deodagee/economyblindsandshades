b32c2c9ec18f093e194b06e5d4a1396089d3aeb9
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2024-02-10 22:00:42 -0500

    Update connectMongoexample.js

--- utils/connectMongoexample.js
+++ utils/connectMongoexample.js
@@ -1,22 +1,18 @@
-// C:\Users\User\economyblindsandshadesjs\utils\connectMongo.js
-
-import { MongoClient } from "mongodb";
+// C:\Users\User\economyblindsandshadesjs\utils\connectMongoexample.js
+import mongoose from "mongoose";
 
 const connectMongo = async () => {
-  const client = new MongoClient(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
   try {
-    await client.connect();
+    await mongoose.connect(process.env.MONGO_URI, {
+      connectTimeoutMS: 30000, // 30 seconds
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     throw error; // Throw the error to be caught in your API route.
-  } finally {
-    await client.close();
   }
 };
 
 export default connectMongo;
+
+
