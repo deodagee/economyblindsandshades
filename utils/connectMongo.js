6e758d4bc4182416f8970d4e1a25a7238a5665f6
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2024-02-27 17:53:39 -0500

    Add files via upload

---
+++ utils/connectMongo.js
@@ -0,0 +1,21 @@
+
+// C:\Users\User\economyblindsandshadesjs\utils\connectMongoexample.js
+import mongoose from "mongoose";
+
+const connectMongo = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      connectTimeoutMS: 30000, // 30 seconds
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error; // Throw the error to be caught in your API route.
+  }
+};
+
+export default connectMongo;
+
+
+
+
