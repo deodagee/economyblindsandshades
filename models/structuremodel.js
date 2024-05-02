95d2898184c04ba34d5cb9417ae95e0e1314230c
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2024-05-02 01:20:09 -0400

    Add files via upload

--- models/structuremodel.js
+++ models/structuremodel.js
@@ -4,50 +4,194 @@ import { Schema, model, models } from "mongoose";
 
 const testSchema = new Schema({
 
-  name: {
+  nameZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  productName1: {
+  productName1ZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  productName2: {
+  productName2ZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  WandPriceCMS: {
+  WandPriceCMSZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  motorizedpriceCMS: {
+  motorizedpriceCMSZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  cordlesspriceCMS: {
+  cordlesspriceCMSZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  roomname: {
+  roomnameZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  inchPricesAfterWidthInsideMount: {
+  inchPricesAfterWidthInsideMountZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  inchPricesAfterHeightInsideMount: {
+  inchPricesAfterHeightInsideMountZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  inchPricesAfterWidthOutsideMount: {
+  inchPricesAfterWidthOutsideMountZebrablindsRoomLightening: {
     type: String,
     required: true,
   },
-  inchPricesAfterHeightOutsideMount: {
+  inchPricesAfterHeightOutsideMountZebrablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+
+  
+
+  nameZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName1ZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName2ZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  roomnameZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountZebrablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  
+
+
+
+  nameShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName1ShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName2ShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  roomnameShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountShangrilablindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+
+
+  
+
+  nameShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName1ShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName2ShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  roomnameShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountShangrilablindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountShangrilablindsRoomLightening: {
     type: String,
     required: true,
   }
+  
 
 });
 
