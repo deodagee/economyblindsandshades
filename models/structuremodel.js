baff57129a523c40711871970809f89cb92ce900
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2024-05-08 00:25:08 -0400

    Add files via upload

--- models/structuremodel.js
+++ models/structuremodel.js
@@ -190,7 +190,417 @@ const testSchema = new Schema({
   inchPricesAfterHeightOutsideMountShangrilablindsRoomLightening: {
     type: String,
     required: true,
-  }
+  },
+
+
+  
+
+  
+
+  nameHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName1HoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName2HoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  roomnameHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountHoneycombBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+
+
+
+
+
+
+
+
+  nameHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName1HoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName2HoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  roomnameHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountHoneycombBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+
+
+
+
+
+
+
+
+
+  nameNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName1NoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName2NoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  roomnameNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountNoDrillTypeBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+
+
+
+
+
+
+
+  nameNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName1NoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName2NoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  roomnameNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountNoDrillTypeBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  
+
+
+
+
+
+
+  nameRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName1RollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName2RollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  roomnameRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountRollerBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+
+
+
+
+
+
+
+  nameRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName1RollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName2RollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  roomnameRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountRollerBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+
+
+
+
+
+
+
+  nameRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName1RomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  productName2RomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  roomnameRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountRomanBlindsRoomLightening: {
+    type: String,
+    required: true,
+  },
+  
+
+
+
+
+
+
+  nameRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName1RomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  productName2RomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  WandPriceCMSRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  roomnameRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  cordlesspriceCMSRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  motorizedpriceCMSRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthInsideMountRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightInsideMountRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterWidthOutsideMountRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
+  inchPricesAfterHeightOutsideMountRomanBlindsRoomDarkening: {
+    type: String,
+    required: true,
+  },
   
 
 });
