3c97fd16cef891e56dbc0284cf76940f72fedc30
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2024-05-29 13:31:15 -0400

    Add files via upload

--- pages/components/pages/checkoutpage.js
+++ pages/components/pages/checkoutpage.js
@@ -2,9 +2,38 @@ import React, { useState, useEffect, useRef } from "react";
 import { loadStripe } from '@stripe/stripe-js';
 import styles from '../../../styles/components/pages/checkoutpage.module.css';
 import Image from 'next/image';
+import FooterPage from "../footer";
+
 
 
 const CheckoutPage = ({ totalPrice }) => {
+
+    ///////////////////////////////// FOOTER ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+    const [showFooter, setShowFooter] = useState(false);
+
+    useEffect(() => {
+
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const documentHeight = document.documentElement.scrollHeight;
+
+            const threshold = 100;
+
+            const isNearBottom = scrollPosition + windowHeight >= documentHeight - threshold;
+
+            setShowFooter(isNearBottom);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -37,102 +66,115 @@ const CheckoutPage = ({ totalPrice }) => {
             const cartData = JSON.parse(cartDataJSON);
 
 
-        setLastSavedsumTotal(parseFloat(cartData.sumTotal));
+            setLastSavedsumTotal(parseFloat(cartData.sumTotal));
 
-    } 
-}, []);
+        }
+    }, []);
 
 
     return (
-        <div className={styles.container}>
-            <div className={styles.price}>
-                <h1>Awesome, your total is ${lastSavedsumTotal}!</h1>
-            </div>
-            <div className={styles.icons}>
-                <Image className={styles.payment_cards_icons} width={100} height={100} src="/paypalcard.png" alt="paypal" />
-                <Image className={styles.payment_cards_icons} width={100} height={100} src="/visacard.png" alt="visa" />
-                <Image className={styles.payment_cards_icons} width={100} height={100} src="/mastercard.png" alt="mastercard" />
-                <Image className={styles.payment_cards_icons} width={100} height={100} src="/amexcard.png" alt="amex" />
-                <Image className={styles.payment_cards_icons} width={100} height={100} src="/maestrocard.png" alt="maestro" />
-            </div>
-            <div className={styles.card__container}>
-                <div className={styles.card}>
-                    <div className={`${styles.row} ${styles.paypal}`}>
-                        <div className={styles.left}>
-                            <input id="pp" type="radio" name="payment" />
-                            <div className={styles.radio}></div>
-                            <label htmlFor="pp">Paypal</label>
-                        </div>
+        <>
+            <div className={styles.container}>
+                <div className={styles.price}>
+                    <h1>Awesome, your total is ${lastSavedsumTotal}!</h1>
+                </div>
+                <div className={styles.icons}>
+                    <Image className={styles.payment_cards_icons} width={100} height={100} src="/paypalcard.png" alt="paypal" />
+                    <Image className={styles.payment_cards_icons} width={100} height={100} src="/visacard.png" alt="visa" />
+                    <Image className={styles.payment_cards_icons} width={100} height={100} src="/mastercard.png" alt="mastercard" />
+                    <Image className={styles.payment_cards_icons} width={100} height={100} src="/amexcard.png" alt="amex" />
+                    <Image className={styles.payment_cards_icons} width={100} height={100} src="/maestrocard.png" alt="maestro" />
+                </div>
+                <div className={styles.card__container}>
+                    <div className={styles.card}>
+                        <div className={`${styles.row} ${styles.paypal}`}>
+                            <div className={styles.left}>
+                                <input id="pp" type="radio" name="payment" />
+                                <div className={styles.radio}></div>
+                                <label htmlFor="pp">Paypal</label>
+                            </div>
 
-                    </div>
-                    <div className={`${styles.row} ${styles.credit}`}>
-                        <div className={styles.left}>
-                            <input id="cd" type="radio" name="payment" />
-                            <div className={styles.radio}></div>
-                            <label htmlFor="cd">Debit/ Credit Card</label>
                         </div>
-                    </div>
-                    <div className={`${styles.row} ${styles.cardholder}`}>
-                        <div className={styles.info}>
-                            <label htmlFor="cardholdername">Name</label>
-                            <input placeholder="e.g. Richard Bovell" id="cardholdername" type="text" />
+                        <div className={`${styles.row} ${styles.credit}`}>
+                            <div className={styles.left}>
+                                <input id="cd" type="radio" name="payment" />
+                                <div className={styles.radio}></div>
+                                <label htmlFor="cd">Debit/ Credit Card</label>
+                            </div>
                         </div>
-                    </div>
-                    <div className={`${styles.row} ${styles.number}`}>
-                        <div className={styles.info}>
-                            <label htmlFor="cardnumber">Card number</label>
-                            <input id="cardnumber" type="text" pattern="[0-9]{16,19}" maxLength="19" placeholder="8888-8888-8888-8888" />
+                        <div className={`${styles.row} ${styles.cardholder}`}>
+                            <div className={styles.info}>
+                                <label htmlFor="cardholdername">Name</label>
+                                <input placeholder="e.g. Richard Bovell" id="cardholdername" type="text" />
+                            </div>
                         </div>
-                    </div>
-                    <div className={styles.row}>
-                        <div className={styles.left}>
-                            <label htmlFor="expiry-date">Expiry</label>
-                            <select id="expiry-date">
-                                <option>MM</option>
-                                <option value="1">01</option>
-                                <option value="2">02</option>
-                                <option value="3">03</option>
-                                <option value="4">04</option>
-                                <option value="5">05</option>
-                                <option value="6">06</option>
-                                <option value="7">07</option>
-                                <option value="8">08</option>
-                                <option value="9">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                            </select>
-                            <span>/</span>
-                            <select id="expiry-date">
-                                <option>YYYY</option>
-                                <option value="2016">2016</option>
-                                <option value="2017">2017</option>
-                                <option value="2018">2018</option>
-                                <option value="2019">2019</option>
-                                <option value="2020">2020</option>
-                                <option value="2021">2021</option>
-                                <option value="2022">2022</option>
-                                <option value="2023">2023</option>
-                                <option value="2024">2024</option>
-                                <option value="2025">2025</option>
-                                <option value="2026">2026</option>
-                                <option value="2027">2027</option>
-                                <option value="2028">2028</option>
-                                <option value="2029">2029</option>
-                                <option value="2030">2030</option>
-                            </select>
+                        <div className={`${styles.row} ${styles.number}`}>
+                            <div className={styles.info}>
+                                <label htmlFor="cardnumber">Card number</label>
+                                <input id="cardnumber" type="text" pattern="[0-9]{16,19}" maxLength="19" placeholder="8888-8888-8888-8888" />
+                            </div>
                         </div>
-                        <div className={styles.right}>
-                            <label htmlFor="cvv">CVC/CVV</label>
-                            <input type="text" maxLength="4" placeholder="123" />
-                            <span data-balloon-length="medium" data-balloon="The 3 or 4-digit number on the back of your card." data-balloon-pos="up">i</span>
+                        <div className={styles.row}>
+                            <div className={styles.left}>
+                                <label htmlFor="expiry-date">Expiry</label>
+                                <select id="expiry-date">
+                                    <option>MM</option>
+                                    <option value="1">01</option>
+                                    <option value="2">02</option>
+                                    <option value="3">03</option>
+                                    <option value="4">04</option>
+                                    <option value="5">05</option>
+                                    <option value="6">06</option>
+                                    <option value="7">07</option>
+                                    <option value="8">08</option>
+                                    <option value="9">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                </select>
+                                <span>/</span>
+                                <select id="expiry-date">
+                                    <option>YYYY</option>
+                                    <option value="2016">2016</option>
+                                    <option value="2017">2017</option>
+                                    <option value="2018">2018</option>
+                                    <option value="2019">2019</option>
+                                    <option value="2020">2020</option>
+                                    <option value="2021">2021</option>
+                                    <option value="2022">2022</option>
+                                    <option value="2023">2023</option>
+                                    <option value="2024">2024</option>
+                                    <option value="2025">2025</option>
+                                    <option value="2026">2026</option>
+                                    <option value="2027">2027</option>
+                                    <option value="2028">2028</option>
+                                    <option value="2029">2029</option>
+                                    <option value="2030">2030</option>
+                                </select>
+                            </div>
+                            <div className={styles.right}>
+                                <label htmlFor="cvv">CVC/CVV</label>
+                                <input type="text" maxLength="4" placeholder="123" />
+                                <span data-balloon-length="medium" data-balloon="The 3 or 4-digit number on the back of your card." data-balloon-pos="up"></span>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div className={styles.button}>
+                    <button type="submit"><i className="ion-locked"></i> Confirm and Pay</button>
+                </div>
             </div>
-            <div className={styles.button}>
-                <button type="submit"><i className="ion-locked"></i> Confirm and Pay</button>
-            </div>
-        </div>
+
+
+            {showFooter && <span className={styles.ZebraBlindsFooter_wrapper}>
+                <div className={`${styles.ZebraBlindsFooter} ${showFooter ? styles.showFooter : ''}`}>
+                    <div className={styles.shipping_divider2}></div>
+                    <FooterPage></FooterPage>
+                    <div className={styles.shipping_divider2}></div>
+
+                </div>
+            </span>}
+
+        </>
 
     );
 };
